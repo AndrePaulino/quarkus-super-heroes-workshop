ee09f7d63cad7e57126e02b022333976fb346a84
André Paulino <andrepaulinobtw@gmail.com>
2024-07-31 20:56:29 -0300

    feat: rest-fights project setup

---
+++ super-heroes/rest-fights/src/test/java/io/apaulino/superheroes/fight/FightResourceIT.java
@@ -0,0 +1,8 @@
+package io.apaulino.superheroes.fight;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class FightResourceIT extends FightResourceTest {
+    // Execute the same tests but in packaged mode.
+}
